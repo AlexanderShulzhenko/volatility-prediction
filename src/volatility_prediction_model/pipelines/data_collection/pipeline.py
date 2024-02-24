e9afa29214da9cb7e76102dc3147bfe7138c393f
AlexanderShulzhenko <shulzhenko2426@gmail.com>
2024-02-24 13:37:36 +0400

    Ruff changed

--- src/volatility_prediction_model/pipelines/data_collection/pipeline.py
+++ src/volatility_prediction_model/pipelines/data_collection/pipeline.py
@@ -3,24 +3,26 @@ from kedro.pipeline import Pipeline, node, pipeline
 from .nodes import clean_master_list, get_candlestick_data, get_trades_data
 
 
-def create_pipeline(**kwargs) -> Pipeline: # type: ignore
-    return pipeline([
-        node(
-            func=clean_master_list,
-            inputs="master_list",
-            outputs="master_list_cleaned",
-            name="clean_master_list",
+def create_pipeline(**kwargs) -> Pipeline:  # type: ignore
+    return pipeline(
+        [
+            node(
+                func=clean_master_list,
+                inputs="master_list",
+                outputs="master_list_cleaned",
+                name="clean_master_list",
             ),
-        node(
-            func=get_candlestick_data,
-            inputs=None,
-            outputs="candlesticks_partitioned",
-            name="candlestick_data_collection"
-        ),
-        node(
-            func=get_trades_data,
-            inputs="master_list_cleaned",
-            outputs="trades_partitioned",
-            name="trades_data_collection"
-        ),
-    ])
+            node(
+                func=get_candlestick_data,
+                inputs=None,
+                outputs="candlesticks_partitioned",
+                name="candlestick_data_collection",
+            ),
+            node(
+                func=get_trades_data,
+                inputs="master_list_cleaned",
+                outputs="trades_partitioned",
+                name="trades_data_collection",
+            ),
+        ]
+    )
